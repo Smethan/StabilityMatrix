e8091375e0fa27008189fea59651b76103b12c08
smethan <ethanhargus@gmail.com>
2025-12-01 20:27:39 -0600

    updated APIfactory.cs

--- StabilityMatrix.Core/Api/ApiFactory.cs
+++ StabilityMatrix.Core/Api/ApiFactory.cs
@@ -1,4 +1,8 @@
-﻿using Refit;
+﻿using System.Net.Http;
+using Microsoft.Extensions.Options;
+using Refit;
+using StabilityMatrix.Core.Handlers;
+using StabilityMatrix.Core.Models.Configs;
 
 namespace StabilityMatrix.Core.Api;
 
@@ -33,16 +37,32 @@ public class ApiFactory : IApiFactory
         IReadOnlyDictionary<string, string>? defaultHeaders
     )
     {
-        var httpClient = httpClientFactory.CreateClient(nameof(T));
-        httpClient.BaseAddress = baseAddress;
+        HttpClient httpClient;
 
-        // Add default headers if provided
-        if (defaultHeaders != null)
+        // If headers are provided, use AuthHeaderHandler to properly inject them
+        if (defaultHeaders != null && defaultHeaders.Count > 0)
         {
-            foreach (var header in defaultHeaders)
+            // Create ComfyServerSettings with the headers
+            var serverSettings = new ComfyServerSettings
             {
-                httpClient.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
-            }
+                Headers = new Dictionary<string, string>(defaultHeaders),
+            };
+
+            // Wrap in IOptions
+            var options = Options.Create(serverSettings);
+
+            // Create handler chain: AuthHeaderHandler -> HttpClientHandler
+            var innerHandler = new HttpClientHandler();
+            var authHandler = new AuthHeaderHandler(options) { InnerHandler = innerHandler };
+
+            // Create HttpClient with the handler chain
+            httpClient = new HttpClient(authHandler) { BaseAddress = baseAddress };
+        }
+        else
+        {
+            // No headers, use the factory as before
+            httpClient = httpClientFactory.CreateClient(nameof(T));
+            httpClient.BaseAddress = baseAddress;
         }
 
         return RestService.For<T>(httpClient, refitSettings);
